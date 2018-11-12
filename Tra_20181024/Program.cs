34d177bbe7ecdcbb9a612268f9ea197fbd6f2e30
Michał Jakubiec <michalpjakubiec@gmail.com>
2018-11-12 01:06:22 +0100

    code refactoring

--- Tra_20181024/Program.cs
+++ Tra_20181024/Program.cs
@@ -11,111 +11,86 @@ namespace Tra_20181024
     {
         static void Main(string[] args)
         {
+            bool verbose = false;
+
             const string filePath = "t_1a.txt";
+            const string pattern = @"(?:{""id"":""|^\s*)(?<id>\d{1,10})" +
+                                   @"(?:"",""product_code"":""|,|\s*)(?<product_code>[A-Z-\d]{1,64})" +
+                                   @"(?:"",""product_name"":""|,|\s*|)(?<product_name>(?:\s?[A-Za-z]){1,255})" +
+                                   @"(?:"",""standard_cost"":""|,|\s*)(?<standard_cost>[\d.]{1,9})" +
+                                   @"(?:"",""list_price"":""|,|\s*)(?<list_price>[\d.]{1,9})" +
+                                   @"(?:"",""reorder_level"":""|,|\s*)(?<reorder_level>\d{1,4})" +
+                                   @"(?:"",""target_level"":""|,|\s*)(?<target_level>\d{1,4})" +
+                                   @"(?:"",""quantity_per_unit"":""|,|\s*)(?<quantity_per_unit>[\d\s-A-Za-z.]{1,128}(?<!\d|\s))" +
+                                   @"(?:"",""discontinued"":""|,|\s*)(?<discontinued>\d{1})" +
+                                   @"(?:"",""minimum_reorder_quantity"":""|,|\s*)(?<minimum_reorder_quantity>\d{1,3})" +
+                                   @"(?:"",""category"":""|,|\s*)(?<category>[A-Za-z&\s]{1,128})(?:""}|,|$)";
 
-            RegexToXmlParser parser = new RegexToXmlParser();
-
-            const string pattern = @"(?:{""id"":""|^\s*)(?<id>\d{1,10})(?:"",""product_code"":""|,|\s*)(?<product_code>[A-Z-\d]{1,64})(?:"",""product_name"":""|,|\s*|)(?<product_name>(?:\s?[A-Za-z]){1,255})(?:"",""standard_cost"":""|,|\s*)(?<standard_cost>[\d.]{1,9})(?:"",""list_price"":""|,|\s*)(?<list_price>[\d.]{1,9})(?:"",""reorder_level"":""|,|\s*)(?<reorder_level>\d{1,4})(?:"",""target_level"":""|,|\s*)(?<target_level>\d{1,4})(?:"",""quantity_per_unit"":""|,|\s*)(?<quantity_per_unit>[\d\s-A-Za-z.]{1,128}(?<!\d|\s))(?:"",""discontinued"":""|,|\s*)(?<discontinued>\d{1})(?:"",""minimum_reorder_quantity"":""|,|\s*)(?<minimum_reorder_quantity>\d{1,3})(?:"",""category"":""|,|\s*)(?<category>[A-Za-z&\s]{1,128})(?:""}|,|$)";
+            //const string patternCsv = @"(?<id>\d+),(?<product_code>[A-Z-\d]+),(?<product_name>[A-Za-z\s]+),(?<standard_cost>[\d.]+),(?<list_price>[\d.]+),(?<reorder_level>\d+),(?<target_level>\d+),(?<quantity_per_unit>[\d\s-A-Za-z.]+),(?<discontinued>\d),(?<minimum_reorder_quantity>\d+),(?<category>[A-Za-z\s&]+),"
+            //const string patternColumns = @"(?<id>[\s\d]{10})(?<product_code>[[\sA-Z-\d-]{64})(?<product_name>[\sA-Za-z]{255})(?<standard_cost>[\s\d.]{9})(?<list_price>[\s\d.]{9})(?<reorder_level>[\s\d]{4})(?<target_level>[\s\d]{4})(?<quantity_per_unit>[\d\s-A-Za-z.]{128})(?<discontinued>\d{1})(?<minimum_reorder_quantity>[\d\s]{3})(?<category>[A-Za-z\s&]{128})"
 
             Regex regex = new Regex(pattern);
 
-            var input = LoadLinesFromFile(filePath);
-            List<string> groupsNamesList = GetGroupsNamesAsList(regex);
+            var input = ReadLinesToList(filePath);
+            var groupsNamesList = regex.GetGroupNames().Where(x => x != "0").ToList();
+
+            XmlHelper xmlHelper = new XmlHelper(groupsNamesList, "products_list");
 
             int errorCount = 0;
 
             for (int i = 0; i < input.Count(); i++)
             {
-                Match match = regex.Match(input[i]);
+                Match match = regex.Match(input.ElementAt(i));
 
                 if (match.Success)
                 {
-                    var parsedLine = MatchGroupsInLine(regex, match);
+                    Console.Write(verbose == true ? $"Match {i + 1}\n" : "");
 
-                    if (parsedLine.Count == groupsNamesList.Count)
+                    var parsedMatch = new List<string>();
+
+                    for (int j = 1; j < match.Groups.Count; j++)
                     {
-                        parser.AddRecord(parsedLine, groupsNamesList);
+                        Console.Write(verbose == true ? $"\t{match.Groups[j]}\n" : "");
+                        parsedMatch.Add(match.Groups[j].Value);
                     }
-                    else
-                    {
-                        Console.WriteLine($"ERROR \t the number of records does not match the number of categories in line {i + 1}");
-                        errorCount += 1;
-                    }
+
+                    xmlHelper.AppendParsedMatch(parsedMatch, "product");
                 }
                 else
                 {
-                    Console.WriteLine($"ERROR \t could not parse line  {i + 1}");
-                    errorCount += 1;
+                    Console.WriteLine($"ERROR: could not parse line {i + 1}");
+                    errorCount++;
                 }
             }
 
-            Console.WriteLine($"Successfully parsed {input.Count - errorCount}/{input.Count} lines");
+            Console.WriteLine();
+            Console.WriteLine($"Successfully parsed {input.Count() - errorCount}/{input.Count()} lines.");
+            Console.Write(errorCount != 0 ? $"There was {errorCount} error(s).\n" : "");
 
-            parser.ExportAsXml("output.xml");
+            xmlHelper.ExportXml("output.xml");
 
             Console.WriteLine();
-            Console.Write(@"Do you want to view the output file? y\n: ");
+            Console.Write(@"Do you want to view the output file? y/n: ");
 
-            var choice = Console.Read();
-
-            if (choice == 121 | choice == 89)
-            {
+            if (Console.ReadKey().Key == ConsoleKey.Y)
                 Process.Start("output.xml");
-            }
         }
 
-        private static List<string> LoadLinesFromFile(string fileName)
+        private static IList<string> ReadLinesToList(string fileName)
         {
-            IEnumerable<string> input = new List<string>();
+            IList<string> input = new List<string>();
 
             if (File.Exists(fileName))
             {
-                input = File.ReadLines(fileName);
+                input = File.ReadLines(fileName).ToList();
             }
             else
             {
                 Console.WriteLine("Input file not found. Quitting...");
-                Environment.Exit(0);
+                Environment.Exit(2);
             }
 
-            return input.ToList();
-        }
-
-        private static List<string> GetGroupsNamesAsList(Regex r)
-        {
-            List<string> groupsNamesList = new List<string>();
-            string[] groupNames = r.GetGroupNames();
-
-            foreach (var name in groupNames)
-            {
-                if (name.All(char.IsDigit) == false)
-                {
-                    groupsNamesList.Add(name);
-                }
-            }
-
-            return groupsNamesList;
-        }
-
-        private static List<string> MatchGroupsInLine(Regex r, Match m)
-        {
-            var parsedRecordsList = new List<string>();
-            string[] groupNames = r.GetGroupNames();
-
-            foreach (var name in groupNames)
-            {
-                Group group = m.Groups[name];
-
-                if (name.All(char.IsDigit) == false)
-                {
-                    var value = group.Value.TrimStart();
-                    parsedRecordsList.Add(value);
-                    //Console.WriteLine($"\t{name,-50}{value,-60}");
-                }
-            }
-            //Console.WriteLine();
-
-            return parsedRecordsList;
+            return input;
         }
     }
 }
---
+++ Tra_20181024/XmlHelper.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+
+namespace Tra_20181024
+{
+    public class XmlHelper
+    {
+        public XmlDocument XmlDoc { get; set; }
+        public XmlNode RootNode { get; set; }
+        public IList<string> CategoriesList { get; set; }
+
+        public XmlHelper(IList<string> categoriesList, string rootName)
+        {
+            CategoriesList = categoriesList;
+
+            XmlDoc = new XmlDocument();
+            RootNode = XmlDoc.CreateElement(rootName);
+            XmlDoc.AppendChild(RootNode);
+        }
+
+        public void AppendParsedMatch(IList<string> matchedRecordsList, string elementName)
+        {
+            var newElement = XmlDoc.CreateElement(elementName);
+            RootNode.AppendChild(newElement);
+
+            for (int i = 0; i < matchedRecordsList.Count(); i++)
+            {
+                XmlNode category = XmlDoc.CreateElement(CategoriesList.ElementAt(i));
+                category.InnerText = matchedRecordsList[i];
+                newElement.AppendChild(category);
+            }
+        }
+
+        public void ExportXml(string fileName)
+        {
+            XmlDoc.Save(fileName);
+        }
+    }
+}
